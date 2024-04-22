af11fed9df4aa7d2b16cf1bfdb6273c3a4583595
Kathryn Reese <kathryn.ivyreese@gmail.com>
2024-04-22 14:07:48 -0600

    organize for submission

--- employeeSQL/employees.sql
+++ employeeSQL/employees.sql
@@ -1,34 +1,40 @@
 CREATE DATABASE sql_challenge;
 
-CREATE TABLE employees(emp_no INT NOT NULL,
+CREATE TABLE employees(emp_no INT PRIMARY KEY NOT NULL,
 					   emp_title_id VARCHAR(20) NOT NULL,
 					   birth_date DATE,
 					   first_name VARCHAR(20),
 					   last_name VARCHAR(20),
 					   sex CHAR,
 					   hire_date DATE
+					   FORIEGN KEY(emp_title_id) REFERENCES title(title_id)
 );
 
 CREATE TABLE salaries(emp_no INT NOT NULL,
-					 salary INT);
+					 salary INT
+					 FORIEGN KEY(emp_no) REFERENCES employees(emp_no));
 					 
-CREATE TABLE departments(dept_no VARCHAR(20) NOT NULL,
+CREATE TABLE departments(dept_no VARCHAR(20) PRIMARY KEY NOT NULL,
 						 dept_name VARCHAR(20)
 );
 
 CREATE TABLE dept_emp(emp_no INT NOT NULL,
 					  dept_no VARCHAR(20) NOT NULL
+					  FORIEGN KEY(emp_no) REFERENCES employees(emp_no)
+					  FORIEGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(dept_no VARCHAR(20),
 						  emp_no INT NOT NULL
+						  FORIEGN KEY(emp_no) REFERENCES employees(emp_no)
+						  FORIEGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE title(title_id VARCHAR(20) NOT NULL,
+CREATE TABLE title(title_id VARCHAR(20) PRIMARY KEY NOT NULL,
 				   title VARCHAR(20)
 );
 
---import all csv files, establish keys via import menu.
+--import all csv files via tables import menu
 
 --ANALYSIS
 --#1 Find the emp #, name, sex, and salary of everyone
